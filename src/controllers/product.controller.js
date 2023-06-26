fd2db2f24b1e4140fda29385a77a67c52c18bde5
HenrySofka <106099521+HenrySofka@users.noreply.github.com>
2023-06-26 19:03:32 -0300

    Se comienza a crear el Controlador de Producto

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,24 @@
+//Métodos[GET]
+const getProductByID = (req, res) => {
+    
+};
+const getProductByCategory = (req, res) => {};
+const getProducts = (req, res) => {};
+
+//Métodos[POST]
+const createProduct = (req, res) => {};
+
+//Métodos[PUT]
+const updateProduct = (req, res) => {};
+
+//Métodos[DELETE]
+const deleteProduct = (req, res) => {};
+
+module.exports = {
+    getProductByID,
+    getProductByCategory,
+    getProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct
+};
