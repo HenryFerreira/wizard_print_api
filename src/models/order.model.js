ab85c540602be7189decbed7c33724352db11a9c
HenrySofka <106099521+HenrySofka@users.noreply.github.com>
2023-06-26 18:17:17 -0300

    Se agrega el UUID para los modelos de la DB

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,17 +1,21 @@
+/*Imports*/
 const mongoose = require("mongoose");
+const uuid = require("uuid");
 
+/*Modelo del Producto*/
 const OrderSchema = new mongoose.Schema(
     {
+        _id      : { type : uuid.v4().toString() },
         title    : { type : String },
         image    : { type : Strign },
         amount   : { type : Number },
         price    : { type : Number },
-        subTotal : { type : Number },
+        subTotal : { type : Number }
     },
     {
-        timeseries: true,
+        timestamps: true,
         versionKey: false
     }
 )
-
+/*Exportar el modelo para la DB*/
 module.exports = mongoose.model("order", OrderSchema);
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,16 +1,20 @@
+/*Imports*/
 const mongoose = require("mongoose");
+const uuid = require("uuid");
 
+/*Modelo del Producto*/
 const ProductSchema = new mongoose.Schema(
     {
+        _id      : { type : uuid.v4().toString() },
         title    : { type : String },
         image    : { type : Strign },
         category : { type : "BUZOS" | "PINES" | "TAZAS" },
         price    : { type : Number}
     },
     {
-        timeseries: true,
+        timestamps: true,
         versionKey: false
     }
 )
-
+/*Exportar el modelo para la DB*/
 module.exports = mongoose.model("products", ProductSchema);
